183acbb4f999c3101636babbb6d73e4901d83793
Fintan Kearney <fintanak@gmail.com>
2023-09-01 00:53:32 +0100

    Adding model for cinema screens

--- libs/cinemas/src/lib/+state/cinemas.models.ts
+++ libs/cinemas/src/lib/+state/cinemas.models.ts
@@ -4,4 +4,10 @@
 export interface CinemasEntity {
   id: string | number; // Primary ID
   name: string;
+  screens: ScreenEntity[];
+}
+
+export interface ScreenEntity {
+  id: string | number; // Primary ID
+  name: string;
 }
