cd6f825319aee0d93652385f8b9dcb09ddb1387f
Fintan Kearney <fintanak@gmail.com>
2023-08-31 23:02:30 +0100

    Angular material

--- apps/ultraplex/src/app/app.module.ts
+++ apps/ultraplex/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@org/material';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 
@@ -9,6 +11,8 @@ import { appRoutes } from './app.routes';
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent],
