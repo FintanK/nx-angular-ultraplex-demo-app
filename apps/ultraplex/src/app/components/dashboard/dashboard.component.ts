d17de9c74d7bac7ddb0ada99229b668efcf3bcfd
Fintan Kearney <fintanak@gmail.com>
2023-09-01 14:32:18 +0100

    Dashboard

--- apps/ultraplex/src/app/components/dashboard/dashboard.component.ts
+++ apps/ultraplex/src/app/components/dashboard/dashboard.component.ts
@@ -1,17 +1,19 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookingsFacade } from '@org/bookings';
 import { CinemasFacade } from '@org/cinemas';
 import { MoviesFacade } from '@org/movies';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'org-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+export class DashboardComponent implements OnInit {
+  allCinemas$ = this.cinemasFacade.allCinemas$;
+  allMovies$ = this.moviesFacade.allMovies$;
+  allBookings$ = this.bookingsFacade.allBookings$;
+
+  numScreens = 0;
 
   constructor(
     public cinemasFacade: CinemasFacade,
@@ -23,24 +25,11 @@ export class DashboardComponent {
     this.moviesFacade.init();
   }
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Cinemas', cols: 1, rows: 1 },
-          { title: 'Screens', cols: 1, rows: 1 },
-          { title: 'Movies', cols: 1, rows: 1 },
-          { title: 'Bookings', cols: 1, rows: 1 },
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-      ];
-    })
-  );
+  ngOnInit(): void {
+    this.allCinemas$.subscribe((cinemas) => {
+      cinemas.forEach((cinema) => {
+        this.numScreens += cinema.screens.length;
+      });
+    });
+  }
 }
